c965678699bec44d8804fbb8398c5268302defd3
Mohamed Taieb Boussoffara <mohamed.taieb.boussoffara@gmail.com>
2022-06-29 14:46:34 +0200

    Save newsletter to Monodb

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,10 +1,22 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
     if (!userEmail || !userEmail.includes("@")) {
       return res.status(422).json({ message: "Invalid email address." });
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://root:root@nextjscluster.3kek9.mongodb.net/?retryWrites=true&w=majority"
+    );
+
+    const db = client.db("newsletter");
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
 
     res.status(201).json({ message: "Signed up!" });
   }
