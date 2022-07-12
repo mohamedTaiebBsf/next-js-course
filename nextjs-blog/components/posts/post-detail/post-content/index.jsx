5a71460c706e15aea0620d2502b1538eab6f4769
Mohamed Taieb Boussoffara <mohamed.taieb.boussoffara@gmail.com>
2022-07-12 14:48:46 +0200

    Add PostDetail page with dummy data

---
+++ nextjs-blog/components/posts/post-detail/post-content/index.jsx
@@ -0,0 +1,25 @@
+import PostHeader from "../post-header";
+import classes from "./styles.module.css";
+
+const DUMMY_POST = {
+  slug: "getting-started-nextjs",
+  title: "Getting started with nextjs",
+  image: "getting-started-nextjs.png",
+  excerpt:
+    "Nextjs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with SSR",
+  date: "202207-12",
+  content: "# This is the title",
+};
+
+function PostContent() {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  );
+}
+
+export default PostContent;
