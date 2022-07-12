75abdab0b9479d2dfdc1270bb461d6f6b75ca736
Mohamed Taieb Boussoffara <mohamed.taieb.boussoffara@gmail.com>
2022-07-12 14:38:58 +0200

    Add AllPosts page with dummy data

---
+++ nextjs-blog/components/posts/all-posts/index.jsx
@@ -0,0 +1,13 @@
+import PostsGrid from "../posts-grid";
+import classes from "./styles.module.css";
+
+function AllPosts({ posts }) {
+  return (
+    <section className={classes.posts}>
+      <h1>All Posts</h1>
+      <PostsGrid posts={posts} />
+    </section>
+  );
+}
+
+export default AllPosts;
--- nextjs-blog/pages/posts/index.jsx
+++ nextjs-blog/pages/posts/index.jsx
@@ -1,3 +1,26 @@
-function AllPostsPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-nextjs",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Nextjs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with SSR",
+    date: "202207-12",
+  },
+  {
+    slug: "getting-started-nextjs2",
+    title: "Getting started with nextjs",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Nextjs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with SSR",
+    date: "202207-12",
+  },
+];
+
+function AllPostsPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostsPage;
