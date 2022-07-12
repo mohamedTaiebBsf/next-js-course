5bc1c2f2d3a771284af467bab5f1a41a31583ba6
Mohamed Taieb Boussoffara <mohamed.taieb.boussoffara@gmail.com>
2022-07-12 15:38:31 +0200

    Rendering Dynamic Post Pages & Paths

--- nextjs-blog/components/posts/post-detail/post-content/index.jsx
+++ nextjs-blog/components/posts/post-detail/post-content/index.jsx
@@ -2,23 +2,13 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "../post-header";
 import classes from "./styles.module.css";
 
-const DUMMY_POST = {
-  slug: "getting-started-nextjs",
-  title: "Getting started with nextjs",
-  image: "getting-started-nextjs.png",
-  excerpt:
-    "Nextjs is the React framework for production - it makes building fullstack React apps and sites a breeze and ships with SSR",
-  date: "202207-12",
-  content: "# This is the title",
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent({ post }) {
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- nextjs-blog/components/posts/post-detail/post-header/index.jsx
+++ nextjs-blog/components/posts/post-detail/post-header/index.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import classes from "./post-header.module.css";
+import classes from "./styles.module.css";
 
 function PostHeader({ title, image }) {
   return (
